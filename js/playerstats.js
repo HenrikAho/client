09297e7f6e004dad804275a21fcf582ed934fb38
Henrik Aho <Henrik.Aho@metropolia.com>
2020-12-03 15:35:38 +0200

    nuin

--- js/playerstats.js
+++ js/playerstats.js
@@ -1,5 +1,5 @@
 let json;
-
+getPlayers();
 function getPlayers() {
         let xmlhttp = new XMLHttpRequest();
         console.log("testiä");
@@ -51,4 +51,77 @@ function showList(json) {
 
         tableBody.appendChild(tableRow);
     }
+}
+
+//----------------------------------------------------------------------------------------------
+
+function getPlayerStats() {
+    let xmlhttp = new XMLHttpRequest();
+    console.log("testiä");
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            json = JSON.parse(xmlhttp.responseText);
+            if (json.numOfRows > 0){ // something found
+                showPlayer(json);
+                console.log("pelaajia löytyi");
+            }
+            else {
+                alert("Pelaajia ei löytynyt!");
+
+            }
+
+        }
+    };
+    xmlhttp.open("GET", "https://rocky-cliffs-72708.herokuapp.com/api/player?group=" + localStorage.getItem("group") + "&player=Onni", true);
+    xmlhttp.send();
+}
+
+function showPlayer(json) {
+    let td1, td2, td3, td4, td5, td6, td7;
+    let string;
+    let pelaaja;
+    let heitotlkm = (json.rows[0].p0 + json.rows[0].p1 + json.rows[0].p2 + json.rows[0].p3 + json.rows[0].p4 + json.rows[0].p5 +
+        json.rows[0].p6 + json.rows[0].p7 + json.rows[0].p8 + json.rows[0].p9 + json.rows[0].p10 + json.rows[0].p11 +
+        json.rows[0].p12);
+
+    let voittoprosentti = (json.rows[0].voitotlkm / json.rows[0].pelatutlkm) * 100;
+    let osumatarkkuus = ((json.rows[0].p1 + json.rows[0].p2 + json.rows[0].p3 + json.rows[0].p4 + json.rows[0].p5 +
+        json.rows[0].p6 + json.rows[0].p7 + json.rows[0].p8 + json.rows[0].p9 + json.rows[0].p10 + json.rows[0].p11 +
+        json.rows[0].p12) / (json.rows[0].p0 + json.rows[0].p1 + json.rows[0].p2 + json.rows[0].p3 + json.rows[0].p4 + json.rows[0].p5 +
+        json.rows[0].p6 + json.rows[0].p7 + json.rows[0].p8 + json.rows[0].p9 + json.rows[0].p10 + json.rows[0].p11 +
+        json.rows[0].p12)) * 100;
+
+    let pistekeskiarvo = (json.rows[0].p0 + json.rows[0].p1 + json.rows[0].p2 * 2 + json.rows[0].p3 * 3 + json.rows[0].p4 * 4 + json.rows[0].p5 * 5 +
+        json.rows[0].p6 * 6 + json.rows[0].p7 * 7 + json.rows[0].p8 * 8 + json.rows[0].p9 * 9 + json.rows[0].p10 * 10 + json.rows[0].p11 * 11 +
+        json.rows[0].p12 * 12) / heitotlkm;
+
+        td1 = document.getElementById("td1");
+        td2 = document.getElementById("td2");
+        td3 = document.getElementById("td3");
+        td4 = document.getElementById("td4");
+        td5 = document.getElementById("td5");
+        td6 = document.getElementById("td6");
+        td7 = document.getElementById("td7");
+
+        string = json.rows[0].nimi;
+        td1.innerHTML = string;
+
+        string = json.rows[0].pelatutlkm;
+        td2.innerHTML = string;
+
+        string = json.rows[0].voitotlkm;
+        td3.innerHTML = string;
+
+        string = voittoprosentti.toFixed(1) + "%";
+        td4.innerHTML = string;
+
+        string = heitotlkm;
+        td5.innerHTML = string;
+
+        string = osumatarkkuus.toFixed(1) + "%";
+        td6.innerHTML = string;
+
+        string = pistekeskiarvo.toFixed(1) + "p";
+        td7.innerHTML = string;
+
 }
