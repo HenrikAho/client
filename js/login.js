981beb10f8fadee1750bc0d847b9a18867d32be7
Tiketin <nikke.tikka@hotmail.com>
2020-12-03 14:06:59 +0200

    Login valmis.

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ function getGroup() {
                 json = JSON.parse(xmlhttp.responseText);
                 if (json.numOfRows > 0){ // something found
                     setGroup(json);
-                    console.log("testin tynkää");
+                    console.log("Ryhmä haettu: " + json.nimi);
                 }
                 else {
                     alert("Ryhmää ei löytynyt!");
@@ -32,4 +32,29 @@ function setGroup(json) {
     localStorage.setItem("group", json.rows[0].nimi);
     console.log(localStorage.getItem("group"));
     location.href = "menuscreen.html";
+}
+function newGroup() {
+    let group = document.getElementById("group").value;
+    let password = document.getElementById("password").value;
+    let body;
+    if (group.length == 0 || password.length == 0) {
+        alert('Täytä molemmat kentät!');
+        return;
+    } else {
+        let pwCheck = window.prompt("Vahvista salasana:");
+        if(password !== pwCheck){
+            alert("Salasana ei täsmää!");
+            return;
+        }
+        else {
+            body = { "nimi": group, "salasana": password};
+            let xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("POST", "https://rocky-cliffs-72708.herokuapp.com/api/newgroup", true);
+            xmlhttp.setRequestHeader("Content-Type", "application/json");
+            xmlhttp.send(JSON.stringify(body));
+            localStorage.setItem("group", body.nimi);
+            console.log(localStorage.getItem("group"));
+            location.href = "menuscreen.html";
+        }
+    }
 }
