d3baa7b11f132e1593191d6fc574fbdb6a47390f
Henrik Aho <Henrik.Aho@metropolia.com>
2020-12-08 20:18:51 +0200

    dark mode kunnossa

--- js/settings.js
+++ js/settings.js
@@ -18,10 +18,8 @@ function changeLightmode() {
 function changeDarkmodeFont() {
     document.getElementsByTagName("h1").item(0).style.color = "white";
     document.getElementsByTagName("h3").item(0).style.color = "white";
-    document.getElementsByTagName("h7").item(0).style.color = "white";
 }
 function changeLightmodeFont() {
     document.getElementsByTagName("h1").item(0).style.color = "black";
     document.getElementsByTagName("h3").item(0).style.color = "black";
-    document.getElementsByTagName("h7").item(0).style.color = "black";
 }
